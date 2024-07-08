c62d2024c6579d3de7f47be172e30d79bb9de459
Alanqwd <nurmatovnurbek555@gmai.com>
2024-07-08 21:07:00 +0300

    дописать код

--- WPF ToDoHW/MainWindow.xaml.cs
+++ WPF ToDoHW/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
-﻿using System.Runtime.CompilerServices;
-using System.Text;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Wpf_5.src;
-using System.ComponentModel;
 
 namespace Wpf_5
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window,INotifyPropertyChanged
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public List<ToDo> todoList;
         public List<ToDo> TodoList
@@ -83,6 +73,6 @@ namespace Wpf_5
             }
             OnPropertyChanged();
         }
-       
+
     }
 }
